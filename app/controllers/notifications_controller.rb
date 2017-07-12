9f229bb9884db619f37351d008b6834284dc7a67
justinweathersby <justinweathersby@gmail.com>
2017-07-12 11:59:28 -0400

    Added controller action for push notifications. updated notification model to send to ionic

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,13 +4,48 @@ class NotificationsController < ApplicationController
     @notifications = Notification.all
   end
 
-  def new
-    @push_notification = Notification.new
-    @users = User.all
-  end
-
   def show
     @notification = Notification.find(params[:id])
   end
 
+  def new
+    @push_notification = Notification.new
+    @users = User.all
+    # @users = User.where.not('device_token' => nil)
+  end
+
+  def edit
+  end
+
+  def create
+    @push_notification = Notification.new(push_notification_params)
+    @push_notification.user_id = current_user.id
+
+    @push_notification.sent_to = params[:tokens]
+    @push_notification.sent_to.each do |t|
+      device_token = User.find(t).device_token
+      @push_notification.tokens.push(device_token) unless device_token.nil?
+    end
+
+    respond_to do |format|
+      if @push_notification.save
+        format.html { redirect_to push_notifications_path, notice: 'Push Notification was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def destroy
+    Notification.find(params[:id]).destroy
+    respond_to do |format|
+      format.html { redirect_to push_notifications_path, notice: 'Push Notification was successfully destroyed.' }
+    end
+  end
+
+  private
+
+  def push_notification_params
+    params.fetch(:push_notification, {}).permit(:message, :sent_to, :user_id, tokens:[])
+  end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,47 @@
+require 'open-uri'
+require 'net/http'
+
 class Notification < ApplicationRecord
+  belongs_to :user
+  after_save :upload_notification_to_ionic
+
+  validates :message, presence: true
+  validates :tokens, presence: true
+
+  # attribute :tokens, :string, array: true
+  # serialize :tokens, Array
+  # serialize :sent_to, Array
+
+private
+  def upload_notification_to_ionic
+    puts "Push Notification Created: Sent-to: self.tokens.to_json"
+
+    params = {
+      "tokens" => self.tokens,
+      "profile" => ENV['IONIC_PUSH_ENV'],
+      "notification":{
+        "message": self.message,
+        "android":{
+          "title": "Crescent Ford",
+          "sound": "true"
+        },
+         "ios": {
+              "title": "Crescent Ford",
+              "sound": "true"
+              # ,
+              # "badge": 1
+            }
+      }
+    }
+
+    uri = URI.parse('https://api.ionic.io/push/notifications')
+    https = Net::HTTP.new(uri.host,uri.port)
+    https.use_ssl = true
+    req = Net::HTTP::Post.new(uri.path)
+    req['Authorization'] = ENV['IONIC_API_TOKEN']
+    req['Content-Type'] = 'application/json'
+    req.body = params.to_json
+    res = https.request(req)
+    puts res.body
+  end
 end
