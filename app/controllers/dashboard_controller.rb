ca42f3e3b6d39edaf868e784a49d50808d661953
justinweathersby <justinweathersby@gmail.com>
2017-07-24 23:38:08 -0400

    Fixed dashboard redirect for non admin users

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,19 +1,13 @@
 class DashboardController < ApplicationController
-  before_action :authenticate_admin
 
   def index
-    @users = User.all
-    @notifications = Notification.all
-    @monthly_growth = ((User.where("created_at > ?", 1.month.ago).count / User.all.count.to_f) * 100).round
-  end
-
-  def authenticate_admin
-    unless current_user.try(:admin?)
-      render json: {
-        status: :unathorized
-      }.to_json
-      # redirect_to new_user_session_url
+    if current_user.try(:admin?)
+      @users = User.all
+      @notifications = Notification.all
+      @monthly_growth = ((User.where("created_at > ?", 1.month.ago).count / User.all.count.to_f) * 100).round
+    else
+      sign_out(current_user)
+      redirect_to "/users/sign_in", notice: "Must be an admin user to login at this dashboard. Please checkout our mobile app for our members."
     end
   end
-
 end
