8e9d15a104bceb931503d05ac195a86a7636c362
justinweathersby <justinweathersby@gmail.com>
2017-07-24 23:28:37 -0400

    Remove blocker from users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 
   before_action :find_user, only: [:show, :edit, :update]
-  before_action :authenticate_admin
 
   def index
     @users = User.all
@@ -14,13 +13,6 @@ class UsersController < ApplicationController
   end
 
   private
-    def authenticate_admin
-      unless current_user.try(:admin?)
-        render json: {
-          status: :unathorized
-        }.to_json
-      end
-    end
 
     def find_user
       @user = User.find(params[:id])
