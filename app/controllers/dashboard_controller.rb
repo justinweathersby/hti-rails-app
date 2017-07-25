10c264cb9f9b3ff122450596a99a2e780eb7bce4
justinweathersby <justinweathersby@gmail.com>
2017-07-24 20:44:23 -0400

    Fix: rounded percentage monthly growth on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,7 @@ class DashboardController < ApplicationController
   def index
     @users = User.all
     @notifications = Notification.all
-    @monthly_growth = (User.where("created_at > ?", 1.month.ago).count / User.all.count.to_f) * 100
+    @monthly_growth = ((User.where("created_at > ?", 1.month.ago).count / User.all.count.to_f) * 100).round
   end
 
   def authenticate_admin
